6a633cec1667dc75d15736f8a2cab5d1aeb3bacc
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-08-13 18:27:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinyl::Application.config.secret_token = '54f4ab08c6211373fe143b1a7e281d2957aadf9593953ca0aab74f34022e21043986515a15b8a00560addd916b7cf8ec26ab2a2890c96a63d6710502273af823'
